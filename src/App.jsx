0afbd6d2ada241da188eb5e6765f7923cf2fda49
JohnsonOdang <johnnyodanny@gmail.com>
2024-01-18 09:55:25 -0800

    creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
+
 
 
 function App() {
 
   return (
+    
     <>
-      <h1 className='text-4xl bg-yellow-600 font-bold underline'>
-        hello world
+      <h1 className="text-center text-4xl bg-slate-300 font-bold underline">
+        Hello World
       </h1>
-
-      <p className='text-xl bg-slate-300'>
-        A React Project
-      </p>
-      
     </>
   )
 }
